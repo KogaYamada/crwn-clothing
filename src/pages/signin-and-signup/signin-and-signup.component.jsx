638bf8110a3f141dfc2e7ac468c7d0e7e53600c4
KogaYamada <koga0087y@gmail.com>
2020-07-11 03:00:04 +0900

    フォームのスタイルとアニメーション

---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./signin-and-signup.style.scss";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
