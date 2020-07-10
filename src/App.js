a2d0d91a99fbdb8606d62000f4e26f6f9d1e8e3e
KogaYamada <koga0087y@gmail.com>
2020-07-10 18:22:18 +0900

    collection-item,collection-previewコンパーネントの追加

--- src/App.js
+++ src/App.js
@@ -3,32 +3,13 @@ import "./App.css";
 import { Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/Homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>hatsPage</h1>
-  </div>
-);
-
-const TopckList = () => (
-  <div>
-    <h1>Topic List Page</h1>
-  </div>
-);
-
-const TopicDetail = () => (
-  <div>
-    <h1>Topic Detail Page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/Shop.component";
 
 function App() {
   return (
     <>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
-      <Route exact path="/topics" component={TopckList} />
-      <Route path="/topics/:topicId" component={TopicDetail} />
+      <Route exact path="/shop" component={ShopPage} />
     </>
   );
 }
