08102a6994794dd155c06f358b6928c9c0bd3a70
KogaYamada <koga0087y@gmail.com>
2020-07-10 16:50:34 +0900

    withRouterのルーティング

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/Homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>hatsPage</h1>
+  </div>
+);
+
+const TopckList = () => (
+  <div>
+    <h1>Topic List Page</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>Topic Detail Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
+      <Route exact path="/topics" component={TopckList} />
+      <Route path="/topics/:topicId" component={TopicDetail} />
+    </>
   );
 }
 
