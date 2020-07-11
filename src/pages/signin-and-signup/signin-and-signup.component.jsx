8bf6ac488f30caaadb9a1521981d250121b3c6cc
KogaYamada <koga0087y@gmail.com>
2020-07-11 16:57:07 +0900

    サインイン機能実装

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
+
 import "./signin-and-signup.style.scss";
 
 const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
