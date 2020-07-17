59cd140f0c2a98364515549c10684347339d5443
KogaYamada <koga0087y@gmail.com>
2020-07-17 18:39:17 +0900

    shopdataのデータ構造の変更
    配列→オブジェクト

--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -1,27 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from "./shopData";
+import CollectionOverview from '../../components/collections-ovewview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => {
-          return <CollectionPreview key={id} {...otherCollectionProps} />;
-        })}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => {
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route
+        exact
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      />
+    </div>
+  );
+};
 
 export default ShopPage;
